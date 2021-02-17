0f3a7511d8c37ca84fb83e3b1767d6ceb1a7853c
Thomas Farvour <tom@farvour.com>
2021-02-17 01:54:57 -0800

    Customize savedir, remove double quote issue.

--- scripts/startserver-1.sh
+++ scripts/startserver-1.sh
@@ -29,12 +29,12 @@ echo "Name: ${VALHEIM_SERVER_NAME}"
 echo "World: ${VALHEIM_SERVER_WORLD}"
 
 SERVER_ARGS=(
-    "-savedir \"${SERVER_DATA_DIR}\"" # This comes from Dockerfile/ENV.
-    "-name \"${VALHEIM_SERVER_NAME}\""
+    "-name ${VALHEIM_SERVER_NAME}"
     "-port 2456"
-    "-world \"${VALHEIM_SERVER_WORLD}\""
-    "-password \"${VALHEIM_SERVER_PASSWORD}\""
+    "-world ${VALHEIM_SERVER_WORLD}"
+    "-password ${VALHEIM_SERVER_PASSWORD}"
     "-public ${VALHEIM_SERVER_PUBLIC}"
+    "-savedir ${SERVER_DATA_DIR}" # This comes from Dockerfile/ENV.
 )
 
 set -x
