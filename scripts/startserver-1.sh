db5cdae7f30f1b1e8a43012eb0f6c2af22067585
Thomas Farvour <tom@farvour.com>
2021-02-17 01:29:00 -0800

    Add custom server data dir.

--- scripts/startserver-1.sh
+++ scripts/startserver-1.sh
@@ -29,11 +29,12 @@ echo "Name: ${VALHEIM_SERVER_NAME}"
 echo "World: ${VALHEIM_SERVER_WORLD}"
 
 SERVER_ARGS=(
+    "-savedir \"${SERVER_DATA_DIR}\"" # This comes from Dockerfile/ENV.
     "-name \"${VALHEIM_SERVER_NAME}\""
     "-port 2456"
     "-world \"${VALHEIM_SERVER_WORLD}\""
     "-password \"${VALHEIM_SERVER_PASSWORD}\""
-    "-public \"${VALHEIM_SERVER_PUBLIC}\""
+    "-public ${VALHEIM_SERVER_PUBLIC}"
 )
 
 set -x
